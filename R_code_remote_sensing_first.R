ecbcc0b9724a8552c72c27caa24e12df3a345b91
Beatrice-aiello25 <80917408+Beatrice-aiello25@users.noreply.github.com>
2021-03-18 23:25:03 +0100

    Update R_code_remote_sensing_first.R

--- R_code_remote_sensing_first.R
+++ R_code_remote_sensing_first.R
@@ -1,2 +1,33 @@
 # il mio primo codice in R per il telerilevamento!
-# install.packages("raster")
+
+# Codice per installazione pacchetti aggiuntivi raster
+install.packages("raster")
+
+# Funzione library per richiamare il pacchetto raster
+library(raster)
+
+# Indicare la cartella da cui estrarre i dati
+setwd(C:/lab/")
+
+# Funzione brick per importare i dati
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+# Funzione per avere informazioni sulle bande
+p224r63_2011
+
+# Funzione plot immagini per visualizzare le varie bande 
+plot(p224r63_2011) 
+
+# Funzione colorRampPalette stabilisce una determinata palette di colori
+cl <- colorRampPalette(c("black", "grey", "light grey"))(100)
+
+# Funzione plot 
+plot(p224r63_2011, col= cl)
+
+# Funzione plot colour change new
+cl <- colorRampPalette(c("blue", "green", "yellow", "red", "light blue"))(100)
+plot(p224r63_2011, col= cl)
+
+# Funzione plot colour change new2
+cl <- colorRampPalette(c("blue", "green", "orange", "red", "light blue", "purple"))(100)
+plot(p224r63_2011, col= cl)
