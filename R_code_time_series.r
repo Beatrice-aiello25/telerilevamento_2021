d491d4f21892d6d32fff969e84128e230fdca95d
Beatrice-aiello25 <80917408+Beatrice-aiello25@users.noreply.github.com>
2021-04-07 13:05:40 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -7,3 +7,39 @@ library(raster)
 
 # Indicare la cartella da cui estrarre i dati
 setwd("C:/lab/greenland") 
+
+# Funzione raster per caricare i singoli dati 
+lst_2000 <- raster ("lst_2000.tif")
+
+# Funzione plot
+plot(lst_2000)
+lst_2005 <- raster ("lst_2005.tif")
+# Funzione plot
+plot(lst_2005)
+lst_2010 <- raster ("lst_2010.tif")
+lst_2015 <- raster ("lst_2015.tif")
+
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+# Funzione list.files crea una lista di file
+list.files()
+
+rlist <- list.files(pattern="lst")
+rlist
+
+# Funzione lapply permette di importare la lista di file e applicarvi la funzione raster
+import <- lapply(rlist,raster)
+
+import
+
+# Funzione stack crea un unico pacchetto di file
+TGr <- stack(import)
+plot(TGr)
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+plotRGB(TGr, 2, 3, 4, stretch="Lin")
+
+
