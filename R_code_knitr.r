1477bfae62109d6bc168f6b442e976b63dbc464d
Beatrice-aiello25 <80917408+Beatrice-aiello25@users.noreply.github.com>
2021-04-16 11:37:32 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -6,7 +6,8 @@ setwd("C:/lab)
 # Funzione library per richiamare il pacchetto knitr
 library(knitr)
 
-
+# Funzione stitch
+stitch("r_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
 
 
