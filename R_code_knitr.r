ba093afd82b765d630a125f4c2993e0bca958272
Beatrice-aiello25 <80917408+Beatrice-aiello25@users.noreply.github.com>
2021-04-20 12:54:12 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -7,10 +7,10 @@ setwd("C:/lab)
 library(knitr)
 
 # Funzione stitch
-stitch("r_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
-
-
-
+stitch("r_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+# viene generato un file tex nella cartella lab
+R_code_greenland.tex
+# apriamo il file con un programma di testo blocco note, codice che verrà poi copiato su overleaf per poter generare un pdf
 
 
 
