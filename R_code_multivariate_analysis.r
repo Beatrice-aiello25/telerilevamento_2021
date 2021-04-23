3a555dfb1346753df4184005deb80b3c706b9378
Beatrice-aiello25 <80917408+Beatrice-aiello25@users.noreply.github.com>
2021-04-23 11:02:10 +0200

    Create R_code_multivariate_analysis.r

---
+++ R_code_multivariate_analysis.r
@@ -0,0 +1,37 @@
+## Analisi multivariata
+
+library(raster)
+library (RStoolbox)
+
+#settaggio working directory
+setwd("C:/lab/") 
+
+# Funzione brick
+p224_2011 <- brick("p224r63_2011_masked.grd")
+plot(p224_2011)
+# mi indica tutte le informazioni sull'immagine
+p224_2011
+
+plot(p224_2011$B1_sre, p224_2011$B2_sre, col="red", pch=19, cex=2)
+plot(p224_2011$B2_sre, p224_2011$B1_sre, col="red", pch=19, cex=2)
+# funzione pairs
+pairs(p224_2011)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
