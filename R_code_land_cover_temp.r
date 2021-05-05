f06fa060346d0fbfb322e03eb1b7b87547e0b3c7
Beatrice-aiello25 <80917408+Beatrice-aiello25@users.noreply.github.com>
2021-05-05 12:49:45 +0200

    Update R_code_land_cover_temp.r

--- R_code_land_cover_temp.r
+++ R_code_land_cover_temp.r
@@ -1,11 +1,34 @@
 ### R_code_land_cover_temp.r
 
+library(raster)
+library(RStoolbox)
+#install.packages("ggplot2") funziona con qualsiasi dataset
+library(ggplot2)
+# Set della working directory 
+setwd("C:/lab/")
 
+# funzione brick per importare i dati
+defor1 <- brick("defor1.jpg)
+# plot dell'immagine defor1
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
 
+defor2 <- brick("defor2.jpg")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
 
+# funzione par
+par(mfrow=c(1,2))
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
 
+#  install.packages("gridExtra")
+library(gridExtra)
 
-
+# multiframe con ggplot2 e gridExtra
+p1 <- ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
+p2 <- ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
+grid.arrange(p1,p2, nrow=2)
 
 
 
