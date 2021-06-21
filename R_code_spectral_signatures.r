43684b44372c2d945ecc8c4f6278c31b8c5f30bc
Beatrice-aiello25 <80917408+Beatrice-aiello25@users.noreply.github.com>
2021-06-21 13:29:48 +0200

    Update and rename R_code_spectral_signatures.rre to R_code_spectral_signatures.r

--- R_code_spectral_signatures.r
+++ R_code_spectral_signatures.r
@@ -3,6 +3,7 @@
 # Funzione library per richiamare il pacchetto raster precedentemente installato
 library(raster)
 library(rgdal)
+library(ggplot2)
 #la funzione sewd (set working directory) serve a richiamare una specifica cartella di lavoro (directory), in questo caso è "lab", inoltre utilizziamo le virgolette perchè usciamo fuori da R
 setwd("C:/lab/")
 # Funzione brick per importare i dati, anche qui utilizziamo le virgolette perchè peschiamo i dati al di fuori di R
@@ -23,17 +24,36 @@ click(defor2, id=T, xy=T, cell=T, type="p", pch=16, cex=4, col="yellow")
 
 ## pixel corso d'acqua
 # defor2.1= 51 riflettanza molto bassa nell'infrarosso
-# defor2.2= 92 
-# defor2.3= 136
+# defor2.2= 92 riflettanza alta in rosso
+# defor2.3= 136 riflettanza ancora più alta nel verde
 
+# costruiamo le colonne del nostro dataset:
+# c sta per characters
+# nella colonna corrispondente a forest e water inseriremo i valori di riflettanza ottenuti corrispondenti a ciascuna banda
+band <- c(1,2,3)
+forest <- c(217,6,20)
+water <- c(51,92,136)
+# creaiamo la nostra tabella, con la funzione data.frame
+spectrals <- data.frame (band,forest, water)
 
+# band forest water
+# 1    1    217    51
+# 2    2      6    92
+# 3    3     20   136
 
+# vado a plottare le firme spettrali con la funzione ggplot
+# aes: estetics
+ggplot(spectrals, aes(x=band)) + geom_line(aes(y=forest), color="green") + geom_line(aes(y=water), color="blue") 
+labs(x="band",y="reflectance")
 
+## Multitemporal 
 
-
-
-
-
+# Funzione brick per importare i dati, anche qui utilizziamo le virgolette perchè peschiamo i dati al di fuori di R
+defor1 <- brick("defor1.jpg")
+# funzione plot
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+# funzione click per seleionare le firme spettrali
+click(defor1, id=T, xy=T, cell=T, type="p", pch=16, cex=4, col="yellow")
 
 
 
